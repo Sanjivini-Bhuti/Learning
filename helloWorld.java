1b3a71e41e02c492105574f6c4019c4b0056a39b
Sanjivini-Bhuti <83968930+Sanjivini-Bhuti@users.noreply.github.com>
2021-06-09 16:59:51 +0530

    added comment

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,7 @@ public class helloWorld
 {
   public static void main(String[] args)
   {
+    // printing info
     System.out.println("Hello World");
     System.out.println("Hello Java");
     System.out.println("Hello Tanu");
